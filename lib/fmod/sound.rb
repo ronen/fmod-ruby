b5d2b537f67e81e0461f7d12b4ccb13dcd517cd6
ronen barzel <ronen@barzel.org>
2011-11-17 10:49:48 +0000

    cleanup & bug fix: create sound as soon as pointer is defined so that sound.length works before sound.play. get rid of separate play_sound and play_stream and have this object wrap only a Sound not a Stream.

--- lib/fmod/sound.rb
+++ lib/fmod/sound.rb
@@ -18,22 +18,6 @@ module FMOD
     end
     
     def play
-      play_stream
-    end
-
-    def play_sound
-      # FMOD_RESULT = FMOD_System_CreateStream(system, "wave.mp3", FMOD_DEFAULT, 0, &sound);
-      error_check FMOD_System_CreateStream(@system_pointer.read_pointer, @file, FMOD_DEFAULT, 0, sound_ptr);
-
-      # FMOD_RESULT = FMOD_System_PlaySound(system, FMOD_CHANNEL_FREE, sound1, 0, &channel);
-      error_check FMOD_System_PlaySound(@system_pointer.read_pointer, FMOD_CHANNELINDEX[:FMOD_CHANNEL_FREE], sound_ptr.read_pointer, 0, @channel.pointer);
-      true
-    end
-    
-    def play_stream  
-      # FMOD_RESULT = FMOD_System_CreateSound(system, "drumloop.wav", FMOD_SOFTWARE, 0, &sound);
-      error_check FMOD_System_CreateSound(@system_pointer.read_pointer, @file, FMOD_DEFAULT, 0, sound_ptr);
-
       # FMOD_RESULT = FMOD_System_PlaySound(system, FMOD_CHANNEL_FREE, sound1, 0, &channel);
       error_check FMOD_System_PlaySound(@system_pointer.read_pointer, FMOD_CHANNELINDEX[:FMOD_CHANNEL_FREE], sound_ptr.read_pointer, 0, @channel.pointer);
       true
@@ -42,7 +26,14 @@ module FMOD
     private
 
     def sound_ptr
-      @sound_ptr ||= FFI::MemoryPointer.new(:pointer)
+      return @sound_ptr if @sound_ptr
+
+      @sound_ptr = FFI::MemoryPointer.new(:pointer)
+      
+      # FMOD_RESULT = FMOD_System_CreateSound(system, "drumloop.wav", FMOD_SOFTWARE, 0, &sound);
+      error_check FMOD_System_CreateSound(@system_pointer.read_pointer, @file, FMOD_DEFAULT, 0, sound_ptr);
+
+      @sound_ptr
     end
 
     
